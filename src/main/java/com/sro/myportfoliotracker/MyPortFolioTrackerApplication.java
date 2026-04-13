362765b2ea9470370a7d9aeb55a736b08b0ae6b3
Sergio Rodriguez Oyola <sergioroy@ext.inditex.com>
2026-04-14 00:03:59 +0200

    fix

--- src/main/java/com/sro/myportfoliotracker/MyPortFolioTrackerApplication.java
+++ src/main/java/com/sro/myportfoliotracker/MyPortFolioTrackerApplication.java
@@ -4,11 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 @SpringBootApplication
 @EnableScheduling
 public class MyPortFolioTrackerApplication {
 
 	public static void main(String[] args) {
+		// Asegurar que el directorio data/ existe antes de que SQLite intente crear la BD
+		try { Files.createDirectories(Path.of("./data")); } catch (final Exception ignored) {}
 		SpringApplication.run(MyPortFolioTrackerApplication.class, args);
 	}
 
